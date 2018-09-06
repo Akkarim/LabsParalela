933d9a24ce21c51429999a54aaa6bc0007f993d0
Akkarim <luisda7cr@gmail.com>
2018-09-05 21:56:49 -0600

    aun no sirve

--- ejercicio3/ejercicio3/mergeSort.cpp
+++ ejercicio3/ejercicio3/mergeSort.cpp
@@ -20,6 +20,7 @@ bool validaCntHilos(int ch);
 //vector<int>* mergesort(int* inicio, int* final, vector<int>* vector);
 
 vector<int> aleatorios;
+vector<int> resultado(100, 0);
 
 int main(int argc, char* argv[]) {
 	int tCount = 0;
@@ -32,56 +33,58 @@ int main(int argc, char* argv[]) {
 		cin >> data_count;
 		cout << endl;
 	}
-
 	aleatorios.reserve(data_count);
 	local_n = data_count / tCount;
+	int k = 0;
 
-#  pragma omp parallel for num_threads(tCount) 
+//#  pragma omp parallel for num_threads(tCount) private(k) //Lo Hace "bien" pero cada hilo genera el mismo número entonces :c
 	for (int i = 0; i < data_count; i++) {
-		aleatorios.push_back(rand() % 100);
+		k = rand() % 100 + 1;
+//#	pragma omp critical
+		cout << k << endl;
+		aleatorios.push_back(k);
 	}
-
 
 #pragma omp parallel num_threads(tCount) private(local_inicio,local_final,my_Rank)
 	{
-
 		my_Rank = omp_get_thread_num();
 		local_inicio = local_n * my_Rank;
 		local_final = local_n + local_inicio;
 		sort((aleatorios.begin() + local_inicio), (aleatorios.begin() + local_final));
-
 	}
+
 	std::cout << "Lista contiene:";
 	for (std::vector<int>::iterator it = aleatorios.begin(); it != aleatorios.end(); ++it)
 		std::cout << ' ' << *it;
-	std::cout << '\n';
+	cout << '\n';
 
 	int i2, f2;
-	int mitad = data_count / 2;
-
-	vector<int> resultado;
+	int mitad = aleatorios.size()/ 2;
 	resultado.reserve(data_count);
+	//cout << resultado.size << endl;
 	local_final = local_n;
-	for (int i = 0; i < tCount-1; i++) {
+
+	/*
+	std::vector<int>::iterator iti = aleatorios.begin();
+	cout << "El inicio es " << *iti << endl;
+	vector<int>::iterator itm = aleatorios.begin() + mitad + 1;
+	cout << "La mitad es " << *itm << endl;
+	*/
+
+	//merge((aleatorios.begin()), (aleatorios.begin() + mitad), (aleatorios.begin() + mitad), (aleatorios.end()), resultado.begin());
+
+	for (int i = 1; i < tCount-1; i++) {
 		i2 = local_n * i;
 		f2 = local_n + i2;
-		merge((aleatorios.begin()),(aleatorios.begin()+mitad),(aleatorios.begin()+mitad),(aleatorios.end()),(aleatorios.begin()));
-	//	merge((aleatorios.begin()), (aleatorios.begin() + local_final), (aleatorios.begin() + i2), (aleatorios.begin() + f2), aleatorios.begin());
+	//	merge((aleatorios.begin()),(aleatorios.begin()+mitad),(aleatorios.begin()+mitad),(aleatorios.end()),(aleatorios.begin()));
+		merge((aleatorios.begin()), (aleatorios.begin() + local_final), (aleatorios.begin() + i2), (aleatorios.begin() + f2), resultado.begin());
 		local_final = f2;
 	}
-//#endif
 	
-
-
-	std::cout << "Lista contiene:";
-	for (std::vector<int>::iterator it = aleatorios.begin(); it != aleatorios.end(); ++it)
+	std::cout << "El resultado es: ";
+	for (std::vector<int>::iterator it = resultado.begin(); it != resultado.end(); ++it)
 		std::cout << ' ' << *it;
 	std::cout << '\n';
-
-
-
-
-
 
 
 	/*
@@ -95,9 +98,6 @@ int main(int argc, char* argv[]) {
 	std::cout << std::endl;
 	*/
 
-
-	cout << aleatorios[2];
-
 	cin >> n;
 	return 0;
 }
@@ -105,60 +105,3 @@ int main(int argc, char* argv[]) {
 bool validaCntHilos(int ch) {
 	return ch >= 1;
 }
-/*
-void AlgoritmosIndexada::mergeSort(lista& l, int inicio, int final) {
-	if (inicio < final) {
-		int cantElem = 0;
-		int min = inicio;
-		int max = final;
-		while (min <= max) {
-			cantElem++;
-			min++;
-		}
-		cantElem = cantElem / 2;
-		int mitad = (inicio - 1) + cantElem;
-		mergeSort(l, inicio, mitad);
-		mergeSort(l, mitad + 1, final);
-
-		merge(l, inicio, mitad, final);
-	}
-}
-
-void AlgoritmosIndexada::merge(lista& l, int inicio, int medio, int final) {
-	lista l2;
-	l2.iniciar();
-	int insertar = 1;
-	for (int i = inicio; i <= final; i++) {
-		l2.insertar(l.recuperar(i), insertar);
-		insertar++;
-	}
-
-	int i = 1;
-	int j = (medio%l2.numElem()) + 1;
-	int mitad = medio % l2.numElem();
-	int max = l2.numElem();
-	int k = inicio;
-
-	while (i <= mitad && j <= max) {
-		if (l2.recuperar(i) <= l2.recuperar(j)) {
-			l.modificarElem(l2.recuperar(i), k);
-			i++;
-		}
-		else {
-			l.modificarElem(l2.recuperar(j), k);
-			j++;
-		}
-		k++;
-	}
-	while (i <= mitad) {
-		l.modificarElem(l2.recuperar(i), k);
-		i++;
-		k++;
-	}
-	while (j <= max) {
-		l.modificarElem(l2.recuperar(j), k);
-		j++;
-		k++;
-	}
-}
-*/
