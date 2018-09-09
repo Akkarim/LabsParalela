218942d70517a00575ba4a2ab5350cfba70c0699
Felipe Carmona <luis.carmonacastro@ucr.ac.cr>
2018-09-09 00:09:48 -0600

    Primera parte paralela
    La segunda sería algo así según lo entiendo... Estoy analizando la vara con los ejemplos de la cola

--- ejercicio4/ejercicio4/peterPachechoFTW.cpp
+++ ejercicio4/ejercicio4/peterPachechoFTW.cpp
@@ -21,7 +21,9 @@ queue_lk <string> cola;
 int main(int argc, char* argv[]) {
 	int n, hConsidores; //Hilos productores, Hilos consumidores
 	int hProductores = 0;
-	string textoParcial;
+	string textoParcial,archivo;
+	int indice;
+	string conversion;
 
 	while (!validaCntHilos(hProductores)) {
 		cout << "Digite la cantidad de Hilos Productores ( >= 1 ): " << endl;
@@ -31,6 +33,30 @@ int main(int argc, char* argv[]) {
 		cout << endl;
 	}
 
+	//------------------------Lee un archivo para Productores en Paralelo---------------------------------------------
+
+
+
+#pragma omp parallel num_threads(hProductores) private(indice)
+	{
+		indice = omp_get_thread_num();
+		ifstream lectura;
+		lectura.open("arch"+to_string(indice)+ ".txt", ios::in);
+		char c = lectura.get();
+		string content; //Lo que se guarda en la pila
+		while (lectura.good()) {
+			content += c;
+			c = lectura.get();
+		}
+		cout << content << endl;
+
+#pragma omp critical		
+		cola.push(content);
+	}
+
+
+
+/*
 	//------------------------Lee un archivo para Productores---------------------------------------------
 	ifstream lectura;
 	lectura.open("arch0.txt", ios::in);
@@ -42,15 +68,42 @@ int main(int argc, char* argv[]) {
 	} 
 	cout << content << endl;
 	cola.push(content);
+*/
 
+	
+#pragma omp parallel num_threads(hConsidores) 
+	{ 	
+		if (!cola.empty()) {
+			if (cola.size() == 1) {
+				cola.set_lock();
+				textoParcial = cola.front().msg;
+				cola.pop();
+				cola.unset_lock();
+			}
+			else {
+				textoParcial = cola.front().msg;
+				cola.pop();
+			}
+#	pragma omp critical
+			{
+				istringstream iss(textoParcial);
+				string token;
+
+				while (getline(iss, token, ' ')) {
+					cout << token << endl;
+				}
+			}
+		}
+	}
 	//------------------------Tokenizador para consumidor-----------------------------------------------------------------
+/*
 	textoParcial = cola.front().msg;
 	istringstream iss(textoParcial);
 	string token;
 	while (getline(iss, token, ' ')) {
 		cout << token << endl;
 	}
-
+*/
 	//-----------------------------fin---------------------------------------------------
 
 
