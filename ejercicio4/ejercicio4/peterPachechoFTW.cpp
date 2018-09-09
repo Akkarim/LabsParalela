0d98cc4fc22a0a5f6877fefdbde9c527f2a9534a
Akkarim <luisda7cr@gmail.com>
2018-09-08 18:22:19 -0600

    Tokenizer y lectura listo

--- ejercicio4/ejercicio4/peterPachechoFTW.cpp
+++ ejercicio4/ejercicio4/peterPachechoFTW.cpp
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <iostream>
+#include <stdio.h>
 #include <vector>
+#include <fstream>
 #include <string>
 #include <omp.h>
 #include <math.h>
 #include <random>
 #include <algorithm>
+#include <sstream>
 //#include"queue_lk.h"
 //#include <yourself>
 //#include <love>
@@ -26,6 +29,39 @@ int main(int argc, char* argv[]) {
 		cout << endl;
 	}
 
+	//------------------------Lee un archivo para Productores---------------------------------------------
+	ifstream lectura;
+	lectura.open("arch0.txt", ios::in);
+	char c = lectura.get();
+	string content; //Lo que se guarda en la pila
+	while (lectura.good()) {
+		content += c;
+		c = lectura.get();
+	} 
+	cout << content << endl;
+
+	//------------------------Tokenizador para consumidor-----------------------------------------------------------------
+	istringstream iss(content);
+	string token;
+	while (getline(iss, token, ' ')) {
+		cout << token << endl;
+	}
+
+	//-----------------------------fin---------------------------------------------------
+
+
+
+
+
+	//----------------------Guarda un archivo--------------------------------------------
+	/*
+	string numero = to_string(hProductores); // Convertir el número a String
+	string nombreArchivo = "arch" + numero + ".txt"; //Formar el nombre del archivo
+	ofstream nombre(nombreArchivo);
+	nombre << c << endl;
+	nombre.close();
+	*/
+	//----------------------------fin----------------------------------------------------
 
 
 
