fad02006a52c8fd42210c3584b32b46a0cbbcd3f
Felipe Carmona <luis.carmonacastro@ucr.ac.cr>
2018-09-11 15:03:39 -0600

    EJ4

--- ejercicio4/ejercicio4/peterPachechoFTW.cpp
+++ ejercicio4/ejercicio4/peterPachechoFTW.cpp
@@ -1,4 +1,4 @@
-//#include <stdlib.h>
+#include <stdlib.h>
 #include <iostream>
 #include <stdio.h>
 #include <vector>
@@ -16,6 +16,7 @@
 using namespace std;
 
 bool validaCntHilos(int ch);
+bool find(vector<int> vector, int id);
 queue_lk <string> cola;
 
 int main(int argc, char* argv[]) {
@@ -27,43 +28,82 @@ int main(int argc, char* argv[]) {
 
 	while (!validaCntHilos(hProductores)) {
 		cout << "Digite la cantidad de Hilos Productores, los consumidores se asignarán según la nececidad ( >= 1 ): " << endl;
-		cin >> hProductores; 
+		cin >> hProductores;
 		cout << "Digite la cantidad de Hilos Consumidores ( >= 1 ): " << endl; // Si lo prueba con 3 hilos funciona más o menos
 		cin >> hConsumidores;
 		cout << endl;
 	}
-
+	//------------------------Asignar hilos--------------------------------------------------------------
+	vector<int> productores;
+	vector<int> consumidores;
+	indice = hProductores + hConsumidores;
+	for (int i = 0; i < indice; i++) {
+		if (i < hProductores) {
+			productores.push_back(i);
+		}
+		else if (i >= hProductores && i < indice) {
+			consumidores.push_back(i);
+		}
+	}
+	//indice = 0;
 	//------------------------Lee un archivo para Productores en Paralelo---------------------------------------------
 
 	string content; //Lo que se guarda en la pila
 	int cont = 0;
 	int cont2 = 0;
-#	pragma omp parallel num_threads(hProductores) private(indice, content, linea) //Puse content como privada
+#	pragma omp parallel num_threads(hProductores+hConsumidores) private(indice, content, linea) //Puse content como privada
 	{
 		indice = omp_get_thread_num();
-		ifstream lectura;
-		lectura.open("arch" + to_string(indice) + ".txt", ios::in);
-		char c = lectura.get();
-		while (lectura.good()) { // Funciona casi como good hay que ver cuál funciona mejor
-			if (c == '.') { // Con \n no estaba funcando, se puede poner otra flag o mejorar
-				cont++;
+		if (find(productores, indice)) {
+			ifstream lectura;
+			lectura.open("arch" + to_string(indice) + ".txt", ios::in);
+			char c = lectura.get();
+			while (lectura.good()) { // Funciona casi como good hay que ver cuál funciona mejor
+				if (c == '.') { // Con \n no estaba funcando, se puede poner otra flag o mejorar
+					cont++;
 #	pragma omp critical
-				{
-					linea = content;
-					cola.push(linea);
-					content = "";
-					linea.clear(); // Para separar las líneas
+					{
+						linea = content;
+						cola.push(linea);
+						content = "";
+						linea.clear(); // Para separar las líneas
+
+					}
+					c = lectura.get();
 				}
-				c = lectura.get();
-			}
-			else {
-				cont2++;
-				content += c;
-				c = lectura.get();
+				else {
+					cont2++;
+					content += c;
+					c = lectura.get();
+				}
 			}
 		}
-		lectura.close();
-	}
+		else{
+			while (!cola.empty()) {
+				if (cola.size() == 1) { //Si se pone <= cambia el comportamiento
+					cola.set_lock();
+					lineaArchivo = cola.front().msg;
+					cola.pop();
+					cola.unset_lock();
+				}
+				else {
+					lineaArchivo = cola.front().msg;
+					cola.pop();
+				}
+
+				istringstream iss(lineaArchivo);
+				string token;
+#	pragma omp critical
+				{
+					while (getline(iss, token, ' ')) {
+						cout << token << endl;
+					}
+					cout << "Impreso por el hilo: " << indice << endl;
+				}
+			}
+		}
+		}
+		//lectura.close();
 
 	cout << "Contador de rayan " << cont << endl;
 	cout << "Contador " << cont2 << endl;
@@ -81,30 +121,27 @@ int main(int argc, char* argv[]) {
 	*/
 
 
-#pragma omp parallel num_threads(hConsumidores)// private(lineaArchivo) 
-	{
+	//#	pragma omp critical
+	//	while (!cola.empty()) {
+	//		if (cola.size() == 1) { //Si se pone <= cambia el comportamiento
+	//			cola.set_lock();
+	//			lineaArchivo = cola.front().msg;
+	//			cola.pop();
+	//			cola.unset_lock();
+	//		}
+	//		else {
+	//			lineaArchivo = cola.front().msg;
+	//			cola.pop();
+	//		}
+	//
+	//		istringstream iss(lineaArchivo);
+	//		string token;
+	//
+	//		while (getline(iss, token, ' ')) {
+	//			cout << token << endl;
+	//		}
+	//	}
 
-#	pragma omp critical
-		while (!cola.empty()) {
-			if (cola.size() == 1) { //Si se pone <= cambia el comportamiento
-				cola.set_lock();
-				lineaArchivo = cola.front().msg;
-				cola.pop();
-				cola.unset_lock();
-			}
-			else {
-				lineaArchivo = cola.front().msg;
-				cola.pop();
-			}
-
-			istringstream iss(lineaArchivo);	
-			string token;
-				
-			while (getline(iss, token, ' ')) {
-				cout << token << endl;
-			}
-		}
-	}
 
 	cin >> n;
 	return 0;
@@ -114,7 +151,14 @@ bool validaCntHilos(int ch) {
 	return ch >= 1;
 }
 
-
+bool find(vector<int> vector, int id) {
+	for (int i = 0; i < vector.size(); i++) {
+		if (vector[i] == id) {
+			return true;
+		}
+	}
+	return false;
+}
 
 
 
